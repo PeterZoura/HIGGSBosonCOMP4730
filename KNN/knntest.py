546e704955c91adb27a33c3e29703f375d57eba3
Peter Zoura <fredw401@gmail.com>
2023-10-18 15:15:15 -0400

    added ROC_AUC score output to knn

--- KNN/knntest.py
+++ KNN/knntest.py
@@ -3,6 +3,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score, GridSearchCV, KFold, RepeatedKFold
 from sklearn.metrics import get_scorer_names
+from sklearn.metrics import roc_auc_score
 import matplotlib.pyplot as plt
 import random
 
@@ -28,22 +29,26 @@ param = {'n_neighbors':list(range(1, 50, 1))}
 gs = GridSearchCV(KNeighborsClassifier(),
                   param,
                   cv=10,
-                  scoring="accuracy")
+                  scoring={"accuracy", "roc_auc"},
+                  refit="accuracy")
 
-#print("Modelling AI KNN with all attributes on 10k")
-print("Modelling AI KNN with all attributes on 100k")
+print("Modelling AI KNN with all attributes on 10k")
+#print("Modelling AI KNN with all attributes on 100k")
 gs.fit(X1train, ytrain)
 print("According to accuracy: " + str(gs.best_params_) + " With score: " + str(gs.best_score_))
 print("Accuracy on test data X1 is: " + str(gs.best_estimator_.score(X1test, ytest)))
+print("ROC_AUC is: " + str(round(100 * roc_auc_score(ytest, gs.best_estimator_.predict(X1test)), 2)))
 
-#print("\nModelling AI KNN with 7 special attributes only on 10K")
-print("\nModelling AI KNN with 7 special attributes only on 100K")
+print("\nModelling AI KNN with 7 special attributes only on 10K")
+#print("\nModelling AI KNN with 7 special attributes only on 100K")
 gs.fit(X2train, ytrain)
 print("According to accuracy: " + str(gs.best_params_) + " With score: " + str(gs.best_score_))
 print("Accuracy on test data X2 is: " + str(gs.best_estimator_.score(X2test, ytest)))
+print("ROC_AUC is: " + str(round(100 * roc_auc_score(ytest, gs.best_estimator_.predict(X2test)), 2)))
 
-#print("\nModelling AI KNN with 22 actual attributes only on 10K")
-print("\nModelling AI KNN with 22 actual attributes only on 100K")
+print("\nModelling AI KNN with 22 actual attributes only on 10K")
+#print("\nModelling AI KNN with 22 actual attributes only on 100K")
 gs.fit(X3train, ytrain)
 print("According to accuracy: " + str(gs.best_params_) + " With score: " + str(gs.best_score_))
 print("Accuracy on test data X2 is: " + str(gs.best_estimator_.score(X3test, ytest)))
+print("ROC_AUC is: " + str(round(100 * roc_auc_score(ytest, gs.best_estimator_.predict(X3test)), 2)))
